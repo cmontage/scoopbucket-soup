aaddaf4c2f81e00c530697850b1be20abd2b576c
CMontage <wlioer.sec@outlook.com>
2025-08-31 01:27:24 +0800

    Remove obsolete application manifests

--- bin/formatjson.ps1
+++
@@ -1,5 +0,0 @@
-#Requires -Version 5.1
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$formatjson = "$env:SCOOP_HOME\bin\formatjson.ps1"
-$dir = "$psscriptroot\..\bucket" # checks the parent dir
-Invoke-Expression -Command "$formatjson -dir $dir $($args | ForEach-Object { "$_ " })"
